a7798729baeece0eded78c3974747863dcabd97b
natsteinmetz <nathalie.steinmetz@gmail.com>
2013-08-10 21:38:02 -0700

    Updated license information

--- app/mailers/ticket_mailer.rb
+++ app/mailers/ticket_mailer.rb
@@ -5,6 +5,7 @@ class TicketMailer < ActionMailer::Base
     @ticket = ticket
     @url  = ticket_url(@ticket)
     mail(to: "helpdesk@nathaliesteinmetz.net",
+         return_path: "e0af7242b75046e21a9eb0e08d08c092+#{ticket.id}@inbound.postmarkapp.com"
          subject: "New ticket received: #{ticket.name}")
   end
 
